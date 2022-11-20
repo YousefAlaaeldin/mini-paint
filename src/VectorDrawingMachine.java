8d8809490960d9e306c078353b5d976c38beb3ff
anasmustafa123 <104536579+anasmustafa123@users.noreply.github.com>
2022-11-20 08:52:01 +0200

    last

--- src/VectorDrawingMachine.java
+++ src/VectorDrawingMachine.java
@@ -265,11 +265,6 @@ public class VectorDrawingMachine extends JFrame implements Node{
         }
     }//GEN-LAST:event_DeleteButtonActionPerformed
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
